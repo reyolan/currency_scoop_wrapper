b159e7e4d4abbd873688452c1113438e412e0bb1
reyolan <reynardolan@gmail.com>
2022-07-18 04:10:14 +0800

    finalize endpoints to be wrapped

--- app/wrappers/CurrencyScoop/api/client.rb
+++ app/wrappers/CurrencyScoop/api/client.rb
@@ -2,24 +2,30 @@ module CurrencyScoop
   module Api
     class Client
       BASE_URL = 'https://newton.now.sh/api/v2'.freeze
-      AUTHENTICATION = "?api_key=#{Rails.application.credentials.currency_scope_token}".freeze
+      AUTHENTICATION = "api_key=#{Rails.application.credentials.currency_scope_token}".freeze
 
-      def currency_rates(base_currency)
-        send_request("/latest#{AUTHENTICATION}&base=#{base_currency}")
+      def currency_rates(base_currency:)
+        send_request("/latest?#{AUTHENTICATION}&base=#{base_currency}")
       end
 
-      def specific_currency_rates(base_currency:, specific_currencies:)
+      def specific_currency_rates(base_currency:, symbols:)
         # %2C is comma
-        # specific_currencies should be an array separated by comma?
-        send_request("/latest#{AUTHENTICATION}&base=#{base_currency}&symbols=#{specific_currencies}")
+        # symbols should be an array separated by comma?
+        send_request("/latest?#{AUTHENTICATION}&base=#{base_currency}&symbols=#{symbols}")
       end
 
-      def supported_currencies(currency_type)
-        send_request("/currencies&type=#{currency_type}")
+      def supported_currencies(currency_type:)
+        send_request("/currencies?#{AUTHENTICATION}&type=#{currency_type}")
       end
 
-      def convert_currency(from:, to:, amount:)
-        send_request("/convert?from=#{from}&to=#{to}&amount=#{amount}")
+      def convert_currency(from_currency_code:, to_currency_code:, amount:)
+        send_request("/convert?#{AUTHENTICATION}from=#{from_currency_code}&to=#{to_currency_code}&amount=#{amount}")
+      end
+
+      def historical_exchange_rate(base_currency:, date:, symbols:)
+        # %2C is comma
+        # symbols should be an array separated by comma?
+        send_request("/historical?#{AUTHENTICATION}&base=#{base_currency}&date=#{date}&symbols=#{symbols}")
       end
 
       private
