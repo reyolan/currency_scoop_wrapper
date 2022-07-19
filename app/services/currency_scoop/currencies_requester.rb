a82af89cbd26e2124be4a3fda412088da9e8716c
reyolan <reynardolan@gmail.com>
2022-07-19 23:29:01 +0800

    change symbol to string

--- app/services/currency_scoop/currencies_requester.rb
+++ app/services/currency_scoop/currencies_requester.rb
@@ -12,7 +12,7 @@ module CurrencyScoop
 
     def request_currencies
       client = CurrencyScoop::Api::Client.new
-      currencies = Rails.cache.fetch(:currencies, expires_in: 1.hour) { client.currencies(@currency_type)['response'] }
+      currencies = Rails.cache.fetch('currencies', expires_in: 1.hour) { client.currencies(@currency_type)['response'] }
       Struct.new(*currencies.keys.map(&:to_sym)).new(*currencies.values)
     end
   end
