50c120ff4ef3325a4e414d0edfffdb65b53d7830
reyolan <reynardolan@gmail.com>
2022-07-21 14:57:57 +0800

    arrange methods

--- app/services/currency_scoop/currency_convert_requester.rb
+++ app/services/currency_scoop/currency_convert_requester.rb
@@ -12,15 +12,15 @@ module CurrencyScoop
 
     private
 
+    def input_zero_if_empty_string(amount)
+      amount.blank? ? '0' : amount
+    end
+
     def request_convert_currency
       client = CurrencyScoop::Api::Client.new
       conversion_data = client.convert_currency(from_currency_code: @from_currency_code, to_currency_code: @to_currency_code,
                                                 amount: @amount)['response']
       conversion_data.to_struct
-    end
-
-    def input_zero_if_empty_string(amount)
-      amount.blank? ? '0' : amount
     end
   end
 end
