c30ed2b644f6a8b4b9adf12c936715532a713104
reyolan <reynardolan@gmail.com>
2022-07-21 15:36:28 +0800

    adjust layout and add mobile view

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -2,6 +2,7 @@ import * as ActiveStorage from "@rails/activestorage";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import "../../assets/stylesheets/application.css";
+import "./scripts/mobile_navigation";
 
 ActiveStorage.start();
 Rails.start();
